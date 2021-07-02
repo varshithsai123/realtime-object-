9d44247704ab7f0a62b3bc48ba492cf0bab17327
varshithsai123 <81618261+varshithsai123@users.noreply.github.com>
2021-07-02 13:55:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+video = "";
+
+function preload()
+{
+    video = createVideo('');
+    video.hide();
+}
+
+function setup()
+{
+    canavs = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw()
+{
+    image(video, 0, 0, 480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
